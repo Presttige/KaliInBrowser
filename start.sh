02980b2827635403836d3b7acb069d4a61f92893
Prestige <filip.lakota@gmail.com>
2025-04-14 11:13:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Start the VNC server
+export USER=root
+export DISPLAY=:1
+
+# Clean old lock files
+rm -rf /tmp/.X*-lock /tmp/.X11-unix/X*
+
+# Kill any old sessions
+pkill Xtightvnc || true
+pkill websockify || true
+
+# Start VNC
 vncserver :1 -geometry 1280x800 -depth 24
+
 
 # Start noVNC (websockify + VNC viewer)
 websockify --web=/usr/share/novnc/ 6080 localhost:5901
+
